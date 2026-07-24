6237c8cb6ae2282dc794ee2cc79565397d21e5df
CaiqueBenks <280539986+CaiqueBenks@users.noreply.github.com>
2026-07-23 22:08:40 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './storage.js'
+import './storage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
