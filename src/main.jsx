48e442a162df02be80bc587d371612a2982771eb
CaiqueBenks <280539986+CaiqueBenks@users.noreply.github.com>
2026-07-23 21:34:30 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { syncPromise } from './storage.js'
+import './storage.js'
 
-syncPromise.then(() => {
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  )
-}).catch(() => {
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  )
-})
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
